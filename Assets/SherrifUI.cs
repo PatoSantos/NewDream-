1f3d9744f9cd50a845fc47fde41791b184a69f1c
RodrigoZert <124531854+RodrigoZert@users.noreply.github.com>
2024-05-25 18:18:56 -0600

    Edificio Sheriff UI - Funcionalidad

---
+++ Assets/SherrifUI.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SherrifUI : MonoBehaviour
+{
+    private int missClickCount;
+    [Header("Menu Basics")]
+    [SerializeField] private GameObject Building;
+    [SerializeField] private GameObject ExitButton;
+    [SerializeField] private GameObject MissButton;
+    [SerializeField] private GameObject Alarm;
+    [Header("Interactable Objects")]
+    [SerializeField] private GameObject C1_Item;
+    [SerializeField] private GameObject IC_Item;
+    [SerializeField] private GameObject C1_Note;
+    [SerializeField] private GameObject IC_Note;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OpenMenu();
+    }
+
+    public void OpenMenu()
+    {
+        missClickCount = 0;
+        Alarm.SetActive(false);
+        MissButton.SetActive(true);
+        ExitButton.SetActive(true);
+        C1_Note.SetActive(false);
+        IC_Note.SetActive(false);
+        C1_Item.SetActive(true);
+        IC_Item.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        Building.SetActive(false);
+    }
+
+    public void C1_Open()
+    {
+        C1_Note.SetActive(true);
+        IC_Note.SetActive(false);
+        C1_Item.SetActive(false);
+        IC_Item.SetActive(false);
+    }
+
+    public void C1_Close()
+    {
+        C1_Note.SetActive(false);
+        IC_Note.SetActive(false);
+        C1_Item.SetActive(true);
+        IC_Item.SetActive(true);
+    }
+
+    public void IC_Open()
+    {
+        C1_Note.SetActive(false);
+        IC_Note.SetActive(true);
+        C1_Item.SetActive(false);
+        IC_Item.SetActive(false);
+    }
+
+    public void IC_Close()
+    {
+        C1_Note.SetActive(false);
+        IC_Note.SetActive(false);
+        C1_Item.SetActive(true);
+        IC_Item.SetActive(true);
+    }
+
+    public void MissClick()
+    {
+        missClickCount++;
+        if (missClickCount >= 3) {
+            Alarm.SetActive(true);
+        }
+    }
+}
